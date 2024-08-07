9450f2948fa718930d0f94db5d93ef0e23880efe
Blackoutburst <blackoutburst@hotmail.com>
2024-08-07 20:39:42 +0200

    Model vertex/index computation

--- src/main/kotlin/dev/blackoutburst/sve/graphics/Model.kt
+++ src/main/kotlin/dev/blackoutburst/sve/graphics/Model.kt
@@ -15,11 +15,13 @@ class Model {
         private val shaderProgram = ShaderProgram(vertexShader, fragmentShader)
     }
 
+    val voxels = mutableListOf<Voxel>()
+
     var vaoId = 0
     var vboId = 0
     var eboId = 0
 
-    var vertices = intArrayOf()
+    var vertices = floatArrayOf()
     var indices = intArrayOf()
 
     init {
@@ -36,7 +38,7 @@ class Model {
 
             // VBO
             glBindBuffer(GL_ARRAY_BUFFER, vboId)
-            val vertexBuffer = stack.mallocInt(vertices.size)
+            val vertexBuffer = stack.mallocFloat(vertices.size)
             vertexBuffer.put(vertices).flip()
             glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW)
 
@@ -61,8 +63,96 @@ class Model {
         }
     }
 
-    fun update() {
+    private fun calculateVertexArray() {
+        val vertexArray = mutableListOf<Float>()
+        voxels.forEach {
+            // TOP
+            vertexArray.addAll(listOf(it.position.x + 0f, it.position.y + 1f, it.position.z + 0f, it.color.r, it.color.g, it.color.b, it.color.a, 0f))
+            vertexArray.addAll(listOf(it.position.x + 1f, it.position.y + 1f, it.position.z + 0f, it.color.r, it.color.g, it.color.b, it.color.a, 0f))
+            vertexArray.addAll(listOf(it.position.x + 1f, it.position.y + 1f, it.position.z + 1f, it.color.r, it.color.g, it.color.b, it.color.a, 0f))
+            vertexArray.addAll(listOf(it.position.x + 0f, it.position.y + 1f, it.position.z + 1f, it.color.r, it.color.g, it.color.b, it.color.a, 0f))
 
+            // FRONT
+            vertexArray.addAll(listOf(it.position.x + 0f, it.position.y + 0f, it.position.z + 0f, it.color.r, it.color.g, it.color.b, it.color.a, 1f))
+            vertexArray.addAll(listOf(it.position.x + 1f, it.position.y + 0f, it.position.z + 0f, it.color.r, it.color.g, it.color.b, it.color.a, 1f))
+            vertexArray.addAll(listOf(it.position.x + 1f, it.position.y + 1f, it.position.z + 0f, it.color.r, it.color.g, it.color.b, it.color.a, 1f))
+            vertexArray.addAll(listOf(it.position.x + 0f, it.position.y + 1f, it.position.z + 0f, it.color.r, it.color.g, it.color.b, it.color.a, 1f))
+
+            // BACK
+            vertexArray.addAll(listOf(it.position.x + 0f, it.position.y + 0f, it.position.z + 1f, it.color.r, it.color.g, it.color.b, it.color.a, 2f))
+            vertexArray.addAll(listOf(it.position.x + 1f, it.position.y + 0f, it.position.z + 1f, it.color.r, it.color.g, it.color.b, it.color.a, 2f))
+            vertexArray.addAll(listOf(it.position.x + 1f, it.position.y + 1f, it.position.z + 1f, it.color.r, it.color.g, it.color.b, it.color.a, 2f))
+            vertexArray.addAll(listOf(it.position.x + 0f, it.position.y + 1f, it.position.z + 1f, it.color.r, it.color.g, it.color.b, it.color.a, 2f))
+
+            // LEFT
+            vertexArray.addAll(listOf(it.position.x + 0f, it.position.y + 0f, it.position.z + 0f, it.color.r, it.color.g, it.color.b, it.color.a, 3f))
+            vertexArray.addAll(listOf(it.position.x + 0f, it.position.y + 1f, it.position.z + 0f, it.color.r, it.color.g, it.color.b, it.color.a, 3f))
+            vertexArray.addAll(listOf(it.position.x + 0f, it.position.y + 1f, it.position.z + 1f, it.color.r, it.color.g, it.color.b, it.color.a, 3f))
+            vertexArray.addAll(listOf(it.position.x + 0f, it.position.y + 0f, it.position.z + 1f, it.color.r, it.color.g, it.color.b, it.color.a, 3f))
+
+            // RIGHT
+            vertexArray.addAll(listOf(it.position.x + 1f, it.position.y + 0f, it.position.z + 0f, it.color.r, it.color.g, it.color.b, it.color.a, 4f))
+            vertexArray.addAll(listOf(it.position.x + 1f, it.position.y + 1f, it.position.z + 0f, it.color.r, it.color.g, it.color.b, it.color.a, 4f))
+            vertexArray.addAll(listOf(it.position.x + 1f, it.position.y + 1f, it.position.z + 1f, it.color.r, it.color.g, it.color.b, it.color.a, 4f))
+            vertexArray.addAll(listOf(it.position.x + 1f, it.position.y + 0f, it.position.z + 1f, it.color.r, it.color.g, it.color.b, it.color.a, 4f))
+
+            // BOTTOM
+            vertexArray.addAll(listOf(it.position.x + 0f, it.position.y + 0f, it.position.z + 0f, it.color.r, it.color.g, it.color.b, it.color.a, 5f))
+            vertexArray.addAll(listOf(it.position.x + 1f, it.position.y + 0f, it.position.z + 0f, it.color.r, it.color.g, it.color.b, it.color.a, 5f))
+            vertexArray.addAll(listOf(it.position.x + 1f, it.position.y + 0f, it.position.z + 1f, it.color.r, it.color.g, it.color.b, it.color.a, 5f))
+            vertexArray.addAll(listOf(it.position.x + 0f, it.position.y + 0f, it.position.z + 1f, it.color.r, it.color.g, it.color.b, it.color.a, 5f))
+        }
+
+        vertices = vertexArray.toFloatArray()
+    }
+
+    fun calculateIndexArray() {
+        val indexArray = mutableListOf<Int>()
+        var offset = 0
+        voxels.forEach {
+            indexArray.addAll(
+                listOf(
+                    // TOP
+                    0 + offset, 2 + offset, 1 + offset,
+                    0 + offset, 3 + offset, 2 + offset,
+
+                    // FRONT
+                    4 + offset, 6 + offset, 5 + offset,
+                    4 + offset, 7 + offset, 6 + offset,
+
+                    // BACK
+                    9 + offset, 10 + offset, 8 + offset,
+                    10 + offset, 11 + offset, 8 + offset,
+
+                    // LEFT
+                    12 + offset, 14 + offset, 13 + offset,
+                    12 + offset, 15 + offset, 14 + offset,
+
+                    // RIGHT
+                    17 + offset, 18 + offset, 16 + offset,
+                    18 + offset, 19 + offset, 16 + offset,
+
+                    // BOTTOM
+                    21 + offset, 22 + offset, 20 + offset,
+                    22 + offset, 23 + offset, 20 + offset,
+                )
+            )
+            offset += 24
+        }
+
+        indices = indexArray.toIntArray()
+    }
+
+    fun addVoxel(voxel: Voxel) {
+        voxels.add(voxel)
+        calculateVertexArray()
+        calculateIndexArray()
+    }
+
+    fun removeVoxel(voxel: Voxel) {
+        voxels.remove(voxel)
+        calculateVertexArray()
+        calculateIndexArray()
     }
 
     fun render() {
---
+++ src/main/kotlin/dev/blackoutburst/sve/graphics/Voxel.kt
@@ -0,0 +1,10 @@
+package dev.blackoutburst.sve.graphics
+
+import dev.blackoutburst.sve.maths.Vector3f
+import dev.blackoutburst.sve.utils.Color
+
+data class Voxel(
+
+    var position: Vector3f,
+    var color: Color
+)
